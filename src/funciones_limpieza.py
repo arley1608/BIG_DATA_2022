f48dcbfcf8756cd79ab25d9a49d0345730f0cc38
arley1608 <rodriguezarley8@gmail.com>
2022-09-29 11:43:17 -0500

    Documentacion de las funciones de limpieza

--- src/funciones_limpieza.py
+++ src/funciones_limpieza.py
@@ -2,10 +2,19 @@ import pandas as pd
 import numpy as np
 import os  
 from pathlib import Path
+import argparse
 
 root_dir = Path('.').resolve()
 
 def get_data(filename):
+    """Carga datos desde la carpeta raw
+    
+    Arg:
+        filename: nombre del archivo guardado en la carpeta raw
+        
+    Return:
+        datos --> pd.dataframe """
+        
     data_dir = 'raw'
     file_path = os.path.join(root_dir, 'data', data_dir, filename)
     
@@ -16,6 +25,14 @@ def get_data(filename):
     return datos
 
 def remover_duplicados_y_nulos(datos):
+    """Remueve datos duplicados y datos nulos de la columna UNIDAD
+        
+    Arg:
+        datos --> pd.dataframe
+        
+    Return:
+        datos --> pd.dataframe """
+        
     print('remover_duplicados_y_nulos')
     datos = datos.drop_duplicates()
     datos.reset_index(inplace = True, drop = True)
@@ -26,6 +43,14 @@ def remover_duplicados_y_nulos(datos):
     return datos
     
 def convertir_str_a_num(datos):
+    """Convierte los datos tipo str de la columna EDAD a datos numericos tipo float
+    
+    Arg:
+        datos --> pd.dataframe
+        
+    Return:
+        datos --> pd.dataframe """
+        
     print('convertir_str_a_num')
     datos['EDAD'] = datos['EDAD'].replace({'SIN_DATO' : np.nan})
     datos['EDAD'] = datos['EDAD'].astype('float')
@@ -34,6 +59,14 @@ def convertir_str_a_num(datos):
     return datos
 
 def corregir_fechas(datos):
+    """Corrige el tipo de dato de las fechas del dataframe a tipo pd.datetime
+    
+    Arg:
+        datos --> pd.dataframe
+        
+    Return:
+        datos --> pd.dataframe """
+        
     print('corregir_fechas')
     datos['FECHA_INICIO_DESPLAZAMIENTO_MOVIL'] = pd.to_datetime(datos['FECHA_INICIO_DESPLAZAMIENTO_MOVIL'], errors='coerce')
     datos['RECEPCION'] = pd.to_datetime(datos['RECEPCION'], errors='coerce')
@@ -42,6 +75,14 @@ def corregir_fechas(datos):
     return datos
 
 def save_data(datos, filename):
+    """Guarda los datos limpios en un nuevo archivo en la carpeta processed
+    
+    Arg:
+        datos --> pd.dataframe
+        filename: nombre del archivo
+        
+    Return:
+        datos --> csv"""
     print('DATOS GUARDADOS')
     
     out_name = 'reporte_' + filename
